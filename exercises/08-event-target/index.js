27e8f3086c8fad5d183596a2848fb2b5ec5075bf
Federico Resano <123256603+fede1525@users.noreply.github.com>
2023-12-08 21:17:33 +0000

    commit

--- exercises/08-event-target/index.js
+++ exercises/08-event-target/index.js
@@ -2,6 +2,7 @@ window.onload = function loadFn() {
 	let containerElm = document.getElementById("container");
 	containerElm.addEventListener("click", function(event) {
 		// Your code here
-		
+		const eventTarget = event.target;
+		alert(eventTarget);
 	});
 };
