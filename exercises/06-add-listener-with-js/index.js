bb0e7784a97ce6dd8a8260d65666006161b0afa3
Federico Resano <123256603+fede1525@users.noreply.github.com>
2023-12-08 21:06:24 +0000

    commit

--- exercises/06-add-listener-with-js/index.js
+++ exercises/06-add-listener-with-js/index.js
@@ -1,7 +1,12 @@
 window.onload = function myLoadFunction() {
 	alert("The website just finished loading!");
 	// Some code here
-	
+	listenerFunction();
 };
 
 // The listener function here
+function listenerFunction(){
+	document.querySelector("#theGreen").addEventListener("click", () =>{
+		alert("woohoo!");
+	});
+}
