81c785066b2083888f3986e1969b14b25e33c84d
Federico Resano <123256603+fede1525@users.noreply.github.com>
2023-12-08 13:58:53 +0000

    commit

--- exercises/02-on-click-Hello-World/index.js
+++ exercises/02-on-click-Hello-World/index.js
@@ -1,1 +1,10 @@
-// Declare your function here
+function myClickFunction() {
+    let htmlString = document.getElementById("string");
+    if (!htmlString) {
+        htmlString = document.createElement("div");
+        htmlString.id = "string";
+        document.body.appendChild(htmlString);
+    }
+
+    htmlString.innerHTML = '<div class="container-fluid text-center"> <h1>Hello World</h1> </div>';
+}
